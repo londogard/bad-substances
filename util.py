6a3ab3c1d6fc2ebbef8c38793896e50b41e7d668
denkhan <dennislondogard@gmail.com>
2022-01-15 11:40:36 +0100

    update list of bad substances

--- util.py
+++ util.py
@@ -28,9 +28,19 @@ pfas = [
 'Polytefum',
 'C9-15 Fluoroalcohol phosphate',
 'C8-18 Fluoroalcohol phosphate',
-'Decafluoropentane'
+'Decafluoropentane',
+'Polyteym',
+'Perfluoroktansulfonat',
+'Perfluoroktansyra',
+'Ammonium C6-16 Perfluoroalkylethyl phosphate',
+'Polyperfluoroethoxymethoxy difluoroethyl PEG phosphate',
+'Polyperfluoromethylisopropyl ether',
+'Perfluorononyl dimethicone',
+'Dimetichonol Fluoralcohol dilinoleic acid',
+'Trifluoropropyl dimethicol',
+'Octafluoropentyl methacrylate'
 ]
-pfas_include = ['perfluoro']
+pfas_include = ['perfluoro', 'polyfluoro']
 
 import streamlit as st
 import easyocr
