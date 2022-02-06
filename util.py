272dc5926847afeeb2b480dded7c0dba51a7f9fa
Hampus Londögård <hampus.londogard@gmail.com>
2022-02-06 14:36:06 +0100

    chore: simplifying code through new Streamlit, adding poetry as dep-manager / venv (#1)
    * Trying a new version, with updated streamlit
    
    * Added a note

--- util.py
+++ util.py
@@ -1,69 +1,55 @@
-hormor_storande = ['Benzophenone-1',
-'Benzophenone-3',
-'BHA',
-'BHT',
-'Butylparaben',
-'Cyclomethicone',
-'Cyclotetrasiloxane',
-'Dimethylcyclosiloxane',
-'Ethylhexyl methoxycinnamate',
-'Propylparaben',
-'Resorcinol',
-'Triclosan',
-'Triphenyl Phosphate']
-plast = ['Acrylate/Styrene copolymer',
-'Polyethylene',
-'Polymethyl methacrylate',
-'Polyethylene terephthalate',
-'Nylon']
+hormor_storande = [
+    "Benzophenone-1",
+    "Benzophenone-3",
+    "BHA",
+    "BHT",
+    "Butylparaben",
+    "Cyclomethicone",
+    "Cyclotetrasiloxane",
+    "Dimethylcyclosiloxane",
+    "Ethylhexyl methoxycinnamate",
+    "Propylparaben",
+    "Resorcinol",
+    "Triclosan",
+    "Triphenyl Phosphate",
+]
+plast = [
+    "Acrylate/Styrene copolymer",
+    "Polyethylene",
+    "Polymethyl methacrylate",
+    "Polyethylene terephthalate",
+    "Nylon",
+]
 maybe_cancer = [
-'Cyclotetrasiloxane',
-'PHMB',
-'Polyaminopropyl biguanide',
-'p-Aminophenol',
+    "Cyclotetrasiloxane",
+    "PHMB",
+    "Polyaminopropyl biguanide",
+    "p-Aminophenol",
 ]
 pfas = [
-'PTFE',
-'Polytef',
-'Polytefum',
-'C9-15 Fluoroalcohol phosphate',
-'C8-18 Fluoroalcohol phosphate',
-'Decafluoropentane',
-'Polyteym',
-'Perfluoroktansulfonat',
-'Perfluoroktansyra',
-'Ammonium C6-16 Perfluoroalkylethyl phosphate',
-'Polyperfluoroethoxymethoxy difluoroethyl PEG phosphate',
-'Polyperfluoromethylisopropyl ether',
-'Perfluorononyl dimethicone',
-'Dimetichonol Fluoralcohol dilinoleic acid',
-'Trifluoropropyl dimethicol',
-'Octafluoropentyl methacrylate'
+    "PTFE",
+    "Polytef",
+    "Polytefum",
+    "benzyl",
+    "C9-15 Fluoroalcohol phosphate",
+    "C8-18 Fluoroalcohol phosphate",
+    "Decafluoropentane",
+    "Polyteym",
+    "Perfluoroktansulfonat",
+    "Perfluoroktansyra",
+    "Ammonium C6-16 Perfluoroalkylethyl phosphate",
+    "Polyperfluoroethoxymethoxy difluoroethyl PEG phosphate",
+    "Polyperfluoromethylisopropyl ether",
+    "Perfluorononyl dimethicone",
+    "Dimetichonol Fluoralcohol dilinoleic acid",
+    "Trifluoropropyl dimethicol",
+    "Octafluoropentyl methacrylate",
 ]
-pfas_include = ['perfluoro', 'polyfluoro']
+pfas_include = ["perfluoro", "polyfluoro"]
 
-import streamlit as st
-import easyocr
-
-def get_bad_words(substances):
-    all_words = [res.lower() for res in (hormor_storande + plast + maybe_cancer + pfas + pfas_include)]
-    ingredientes = [res.lower() for res in substances]
-    print("substances", substances)
-    print("Ingredients", ingredientes)
-    bad_substances = []
-    for ingredient in ingredientes:
-        for x in [danger for danger in all_words if danger in ingredient]:
-            bad_substances.append(x)
-    return bad_substances
-
-def print_bad_subtances(bad_substances):
-    if bad_substances:
-        st.text("Your table of contents include substances that are not recommended: ")
-        for substance in bad_substances:
-            st.text(substance)
-    else:
-        st.text("We can't find any substance that is not recommended.")
-
-@st.cache(allow_output_mutation=True)
-def getReader():
-    return easyocr.Reader(['sv'], gpu = False)
+bad_substances = set(
+    [
+        res.lower()
+        for res in (hormor_storande + plast + maybe_cancer + pfas + pfas_include)
+    ]
+)
