e56fcf69d86411e1d5138b1d02145664e3fd051f
Lundez <hampus.londogard@gmail.com>
2022-07-20 12:24:56 +0200

    WIP 2.0

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from doctr.utils.visualization import visualize_page
 
 from numpy import ndarray
 
+from util import get_bad_substances
+
 TWO_HOURS = 60 * 60 * 2
 
 
@@ -17,7 +19,7 @@ def read_files(img: bytes) -> list[ndarray]:
     return DocumentFile.from_images(img)
 
 
-@st.experimental_memo(ttl=TWO_HOURS)
+@st.experimental_memo
 def get_predictor() -> OCRPredictor:
     return ocr_predictor(pretrained=True, assume_straight_pages=True)
 
@@ -25,13 +27,14 @@ def get_predictor() -> OCRPredictor:
 @st.experimental_memo(ttl=TWO_HOURS)
 def predict_texts(data: list[ndarray]):
     predictor = get_predictor()
-    result = predictor(data)
-    fig = visualize_page(result.pages[0].export(), data[0], interactive=False)
-    st.pyplot(fig)
-    # st.write(result.show(data))
-    json_export = result.export()
 
-    return json_export  # TODO draw this appealing.
+    return predictor(data)  # TODO draw this appealing.
+
+
+@st.experimental_memo(ttl=TWO_HOURS)
+def visualize_result(result, data):
+    fig = visualize_page(result.pages[0].export(), data[0], interactive=False)
+    return st.pyplot(fig)
 
 
 def main():
@@ -46,11 +49,27 @@ def main():
     elif mode == "Text":
         text = st.text_area("Insert text")
 
+    bad_substances = get_bad_substances()
+
+    # st.write(bad_substances)
     if img is not None:
         data = read_files(img.getvalue())
         data = predict_texts(data)
+        result_json = data.export()
+        st.write(result_json)
+        words = []
+        for i in range(len(data.pages)):
+            for block in result_json["pages"][i]["blocks"]:
+                for line in block["lines"]:
+                    for word in line["words"]:
+                        words.append(word["value"])  # Dropping confidence etc..
 
-        st.write(data)
+        bad = set(words).intersection(bad_substances)
+        if len(bad):
+            st.write(f"**Bad Substances Found:** {', '.join(bad)}")
+        else:
+            st.write("LGTM")
+        # st.write(result_json["pages"][i])
 
         # text = [
         #    x[1] for x in data
@@ -58,12 +77,12 @@ def main():
         # text = " ".join(text)
         # st.image(img, width=300)
 
-    # if text:
-    #    text = text.replace(",", " ").lower()
-    #    text = text.split(" ")#
-    # bad = set(text).intersection(bad_substances)
-    # if len(bad):
-    #    st.write(f"**Bad Substances Found:** {', '.join(bad)}")
+    if text:
+        text = text.replace(",", " ").lower()
+        text = text.split(" ")
+        bad = set(text).intersection(bad_substances)
+        if len(bad):
+            st.write(f"**Bad Substances Found:** {', '.join(bad)}")
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 import json
-
+import streamlit as st
 
 # reverse dict to retrieve risk/family/use..
 # get set of data
+@st.experimental_memo
 def get_bad_substances() -> set[str]:
-    bad_substances_full = json.load("chemicals.json")
+    with open("chemicals.json") as f:
+        bad_substances_full = json.load(f)
     bad_substances = set()
     for family in bad_substances_full:
         substances = [x.lower() for x in family["substances"]]
