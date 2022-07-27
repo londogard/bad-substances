e5327fa221f16e838bf0fb7ca17120f6d4e1dcca
Lundez <hampus.londogard@gmail.com>
2022-07-27 17:50:29 +0200

    Add sponsor/coffee

--- main.py
+++ main.py
@@ -51,13 +51,25 @@ def img_to_text_box_json(img: bytes) -> tuple[dict, list[str], ndarray]:
     return result, words, imgs[0]
 
 
+def sponsor_section():
+    c1, c2 = st.columns(2)
+    c1.markdown(
+        """<a href="https://www.buymeacoffee.com/hlondogard" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 35px;" ></a>""",
+        unsafe_allow_html=True,
+    )
+    c2.markdown(
+        """<div style="display: flex; align-items: center;"><iframe src="https://github.com/sponsors/Lundez/button" title="Sponsor Lundez" height="35" width="116" style="border: 0;"></iframe><div>&nbsp;on GitHub</div></div>""",
+        unsafe_allow_html=True,
+    )
+
+
 def main():
     """Main function of the App"""
     img, words = None, None
 
+    sponsor_section()
     st.header("Bad Substances - Find your enemies!")
     c1, c2 = st.columns(2)
-
     with c1:
         mode = st.radio("Select Mode", ["Image-file", "Text", "Camera"])
     with c2:
@@ -96,7 +108,7 @@ def main():
             fig = result_to_fig(img_json, img_ndarray)
             c2.pyplot(fig)
     else:
-        return st.write("**No Bad Substances Found**")
+        st.write("**No Bad Substances Found**")
 
 
 if __name__ == "__main__":
